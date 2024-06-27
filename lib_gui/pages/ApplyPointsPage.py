6d8569bcc3a4c4843d923162e4c02ce74d818525
Nicholas Lanotte <35313792+Nick-L-1996@users.noreply.github.com>
2024-06-26 21:16:07 -0400

    added set up functions for the apply points page

--- lib_gui/pages/ApplyPointsPage.py
+++ lib_gui/pages/ApplyPointsPage.py
@@ -11,9 +11,34 @@ __email__ = "nickjl0809@gmail.com"
 from ..page import Page
 
 
-def switch_to_ApplyPointsPage(self):
-    """Switch to confirmation page"""
+def switch_to_ApplyPointsPage(self, user_name: str, user_point_count: int, value_per_point: int, cost_of_test: int):
 
+    self.Welcome_lbl.setText("Welcome " + user_name)
+    self.User_Total_Points_lbl.setText("You have " + str(user_point_count) + " points")
+
+    points_for_free_test = round(float(cost_of_test) / float(value_per_point))
+
+    user_point_count_float = float(user_point_count)
+
+    enableTenPercentDiscount = points_for_free_test/10.0 <= user_point_count_float
+    self.Discount_10_Percent_Button.setEnabled(enableTenPercentDiscount)
+
+    enable25PercentDiscount = points_for_free_test/4.0 <= user_point_count_float
+    self.Discount_25_Percent_Button.setEnabled(enable25PercentDiscount)
+
+    enable50PercentDiscount = points_for_free_test / 2.0 <= user_point_count_float
+    self.Discount_50_Percent_Button.setEnabled(enable50PercentDiscount)
+
+    enable100PercentDiscount = points_for_free_test <= user_point_count_float
+    self.Discount_100_Percent_Button.setEnabled(enable100PercentDiscount)
+
+    if enable100PercentDiscount:  # if the user has enough points for a free test, no point in allowing them to apply the max amount of points. just hide the button
+        self.Apply_Max_Points_Button.hide()
+        self.Apply_Max_Points_Button.setEnabled(False)
+
+
+
+    """Switch to confirmation page"""
     self._switch_to_page(Page.APPLYPOINTS)
 
 
@@ -30,3 +55,5 @@ def Confirm_PointsApplied(self):
 
 def Skip_PointsApplied(self):
     self._switch_to_page(Page.INSTRUCTIONS)
+
+
