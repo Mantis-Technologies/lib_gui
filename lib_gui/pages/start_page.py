8785abea922c6c14ad7f4eb549b6aa816ab17545
Nicholas Lanotte <35313792+Nick-L-1996@users.noreply.github.com>
2024-07-31 20:23:33 -0400

    added debug statement to pressing start button

--- lib_gui/pages/start_page.py
+++ lib_gui/pages/start_page.py
@@ -9,6 +9,7 @@ __maintainer__ = "Justin Furuness"
 __email__ = "jfuruness@gmail.com"
 
 from ..page import Page
+from datetime import datetime
 
 
 def switch_to_start_page(self):
@@ -17,9 +18,16 @@ def switch_to_start_page(self):
     self._switch_to_page(Page.START)
 
 
+def OnBeginButtonPressed(self):
+    current_time = datetime.now().strftime("%b %d %H:%M:%S")
+    message = "Begin Button Pressed"
+    # Print message with current time
+    print(f"{current_time} : {message}")
+    self.switch_to_disclaimer_page()
+
 
 def connect_start_buttons(self):
     """Connects the start page begin button"""
 
-    self.begin_btn.clicked.connect(self.switch_to_disclaimer_page)
+    self.begin_btn.clicked.connect(self.OnBeginButtonPressed)
     self.about_btn.clicked.connect(self.switch_to_about_page)
