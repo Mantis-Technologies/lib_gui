8484121db94474dbffeb54f6237b6e37285983bd
Nicholas Lanotte <35313792+Nick-L-1996@users.noreply.github.com>
2024-12-15 19:32:53 -0500

    removing unneeded code

--- lib_gui/lab_gui.py
+++ lib_gui/lab_gui.py
@@ -17,30 +17,8 @@ class LabGUI(GUI):
     def __init__(self, debug=False, fake_backend: bool = False, *args, **kwargs):
         """Inits gui and modifies for Lab"""
         super(LabGUI, self).__init__(debug=debug, fake_backend=fake_backend, *args, **kwargs)
-        # Override unnecessary components
-        #print("LabGUI: Overriding unnecessary components")
-        #self.override_unused_components()
 
 
-    # Override unused methods
-    def override_unused_components(self):
-        """Skip connecting unused components"""
-        self.connect_faq_buttons = lambda: print("Skipping FAQ buttons for LabGUI")
-        self.connect_leaderboard_buttons = lambda: print("Skipping leaderboard buttons for LabGUI")
-        self.setup_leaderboard_page = lambda _: print(
-            "Skipping leaderboard setup for LabGUI")  # use _: for the overwitten unused componentd with ui version arguments
-        self.setup_faq_page = lambda _: print(
-            "Skipping FAQ setup for LabGUI")  # use _: for the overwitten unused componentd with ui version arguments
-        self.connect_apply_points_page_buttons = lambda: print("Skipping apply points page buttons for LabGUI")
-        self.setup_qr_code_label = lambda _: print(
-            "Skipping QR code label setup for LabGUI")  # use _: for the overwitten unused componentd with ui version arguments
-        self.setup_results_url = lambda _: print(
-            "Skipping results URL setup for LabGUI")  # use _: for the overwitten unused componentd with ui version arguments
-        self.setup_pie_chart = lambda _: print(
-            "Skipping pie chart setup for LabGUI")  # use _: for the overwitten unused componentd with ui version arguments
-
-    def connect_maintenance_buttons(self):
-        print("Skipping connect maintenance buttons.")
 
     @override
     def switch_to_confirmation_page(self):
